5fb43d4cf1eefd97ff663bae938e9306309eba28
seilylook@naver.com <seilylook@naver.com>
2021-01-24 20:00:02 +0900

    7 commit

--- src/main/java/ToyProject/ToyProject_V1/controller/MemberController.java
+++ src/main/java/ToyProject/ToyProject_V1/controller/MemberController.java
@@ -3,6 +3,7 @@ package ToyProject.ToyProject_V1.controller;
 import ToyProject.ToyProject_V1.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -10,6 +11,13 @@ public class MemberController {
     private final MemberService memberService;
 
     public MemberController(MemberService memberService) {
+
         this.memberService = memberService;
+    }
+
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+
+        return "members/createMemberForm";
     }
 }
